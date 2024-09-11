16941afea6393aa6c2b74bfdad964e54162c0c3b
Matt Spahr <matt.spahr@matt.spahr-M4MGYYK3WC>
2024-09-11 15:51:54 -0500

    added random string

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ module "hello-world" {
   source  = "app.terraform.io/mattspahr-sandbox/hello-world/aws"
   version = "1.0.1"
 }
+
+resource "random_string" "test_string" {
+  length = var.string_length
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "string_length" {
+  type        = string
+  description = "Length of random string"
+}
