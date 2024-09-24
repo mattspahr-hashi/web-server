c2c2fa457c4dfb63650cd76f5671ebda8c3a2816
mattspahr-hashi <matt.spahr@hashicorp.com>
2024-09-24 11:31:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "hello-world" {
   source  = "app.terraform.io/mattspahr-sandbox/hello-world/aws"
   version = "1.0.6"
 
-  instance_type = "t2.large"
+  instance_type = "t2.micro"
   instance_name = "${var.env}-hello-world-${random_string.instance_id.result}"
 }
 
