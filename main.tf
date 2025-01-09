2c19da12ec4d7635c76a9a0c2d6f5c4ae803affa
Matt Spahr <matt.spahr@matt.spahr-M4MGYYK3WC>
2025-01-08 21:38:27 -0600

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,40 @@
-module "hello-world" {
-  source  = "app.terraform.io/mattspahr-sandbox/hello-world/aws"
-  version = "1.0.8"
+resource "aws_instance" "hello_world" {
+  ami             = data.hcp_packer_artifact.hello-world.external_identifier
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.web_sg.name]
 
-  instance_type = "t2.micro"
-  instance_name = "${var.env}-hello-world-${random_string.instance_id.result}"
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "${var.instance_name}-sg"
+  description = "Allow inbound traffic to web servers"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "random_string" "instance_id" {
   length  = 16
   special = false
+}
+
+data "hcp_packer_artifact" "hello-world" {
+  bucket_name  = "hello-world"
+  channel_name = "latest"
+  platform     = "aws"
+  region       = "us-east-2"
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "env" {
   type        = string
   description = "Environment"
 }
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 Instance Type"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "Name of EC2 Instance"
+}
