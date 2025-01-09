3ba5e7d012ed8ba8c41a7563925744d925bb3065
Matt Spahr <matt.spahr@matt.spahr-M4MGYYK3WC>
2025-01-08 21:41:28 -0600

    removed variable

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "hello_world" {
   ami             = data.hcp_packer_artifact.hello-world.external_identifier
-  instance_type   = var.instance_type
+  instance_type   = "t2.micro"
   security_groups = [aws_security_group.web_sg.name]
 
   tags = {
